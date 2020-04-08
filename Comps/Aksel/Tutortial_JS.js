3c16ebcba9835f7745b746e8b06f076c982d9172
Aksel <roughsedge.a@gmail.com>
2020-04-07 17:23:28 -0700

    Created the array for advancing the tutorial pages

---
+++ Comps/Aksel/Tutortial_JS.js
@@ -0,0 +1,26 @@
+var tutorial_array = [{
+    picture: `<div class="tutorial_imgContainer">
+                <div class="tutorial_pg1_subtitle">What type of house do you live in?</div>
+                    <div class="tutorial_pg1_imgContainer">
+                    <img class="tutorial_pg1_img1" src="/Imgs/House.svg">
+                    <img class="tutorial_pg1_img2" src="/Imgs/Hand.svg">
+                    <img class="tutorial_pg1_img3" src="/Imgs/Apartment.svg">
+                </div>
+            </div>`,
+    description: `<div class="tutorial__texts">
+                    <div class="tutorial__texts__header">Answer the following questions</div>
+                    <div class="tutorial__texts__description">By answering these series of questions, PlanTree will help you find the plant you desire!</div>
+                </div>`,
+    tutorialProgress:`<div class="tutorial__progress__circles">
+                        <img src="/Imgs/progress_circles1.svg">
+                    </div>`,
+    button: `<div class="tutorial_nextButton">Next</div>`
+},
+
+{
+    picture:``,
+    description:``,
+    tutorialProgress:``,
+    button:``
+}
+]
