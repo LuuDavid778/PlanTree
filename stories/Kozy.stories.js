8015a74c811d8b14963f95d80bdb77520a1fa971
= <=>
2020-04-11 16:14:38 -0700

    FInished CustomButton

--- stories/Kozy.stories.js
+++ stories/Kozy.stories.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import CustomButton from '../Comps/Kozy/CustomButton';
+import ProgressCircles from '../Comps/Kozy/ProgressCircles';
+
 export default {
-    title: "Custom button",
+    title: "Custom Button",
     component: CustomButton
 }
+
 export const Custom_Button = () => <CustomButton></CustomButton>;
+export const Progress_Circle = () => <ProgressCircles img={ProgressCircles2}></ProgressCircles>;
+
+const ProgressCircles2 = require('../../../Imgs/progress_circles2.svg');
