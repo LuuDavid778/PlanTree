a088ca226ffe102a98182c7a9064f1109c38fe53
luudavid778 <luudavid778@gmail.com>
2020-04-11 15:47:31 -0700

    Started startpage_img

---
+++ Comps/David/startpage_img/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../startpage_img.css';
+import '../../../Imgs/'
+const startpage_img = () => <div className = "startpage_img_container">
+<div>
+</div>
+
+</div>
+export default startpage_img;
