339cbe4e495eb70173176470a1d104444c338981
Aksel <roughsedge.a@gmail.com>
2020-04-15 16:12:58 -0700

    Added Aksel.stories.js

---
+++ stories/Aksel.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import TutorialMiddle from '../Comps/Aksel/TutorialMiddle';
+
+export default {
+    title: 'Aksel',
+  };
+
+  export const MyTutorialMiddle = () => <TutorialMiddle />
