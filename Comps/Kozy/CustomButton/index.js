541adf6e2736e24f4f3942ae2cf94fafd21df3be
Aksel <roughsedge.a@gmail.com>
2020-04-20 01:14:42 -0700

    Finished Tutorial page

--- Comps/Kozy/CustomButton/index.js
+++ Comps/Kozy/CustomButton/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "./custombutton.css";
 
 const CustomButton = () => <div>
-    <div>
+    <div className="CustomButton__container">
         <div class="CustomButton" onclick="advance_tutorial_page()">Next</div>
     </div>
 </div>
