d6f78505e0a4b690e69da7eec9f721212a9c4a23
= <=>
2020-04-22 19:07:23 -0700

    Modified buttom in the plant page and merged

--- pages/PlantPage/index.js
+++ pages/PlantPage/index.js
@@ -3,17 +3,16 @@ import './plantpage.css';
 import PlantTopbar from "../../Comps/David/plantpage_topbar";
 import PlantPageQuizCard from "../../Comps/Kozy/PlantPageQuizCard";
 import CustomButtom from "../../Comps/Kozy/CustomButton";
+import HamburgerMenu from "../../Comps/Kozy/HamburgerMenu";
 
 const AptCard = require('../../Imgs/apartment.svg');
 const NextArrow = require('../../Imgs/next_arrow_white.svg');
-
 
 const FirstPlantPage=() =>  <div>
     <PlantTopbar></PlantTopbar>
     <div className="plant_page_card_container">
         <PlantPageQuizCard className="plant_page_card"></PlantPageQuizCard>
         <PlantPageQuizCard icon={AptCard}></PlantPageQuizCard>
-        
     </div>
     <div className="plant_page_card_button">
         <CustomButtom width="180px" text="Next"></CustomButtom>
