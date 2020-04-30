9fbcde756e0a35f8fd0e3668fb8e611a851767a6
Aksel <roughsedge.a@gmail.com>
2020-04-30 13:23:36 -0700

    linked pages

--- Comps/Aksel/GlossaryHeader/index.js
+++ Comps/Aksel/GlossaryHeader/index.js
@@ -5,17 +5,28 @@ const information = require('../../../Imgs/information.svg');
 const glossary = require('../../../Imgs/glossary.svg');
 
 function ClickHeadTutorial(){
-    Router.push("/TutorialPage")
+    Router.push("/TutorialPage01")
 }
 
+function onClick1(){
+    Router.push("/TreeGlossaryPage");
+}
+
+function onClick2(){
+    Router.push("/GlossaryPage");
+}
+
+function onClick3(){
+    Router.push("/TerminologyGlossaryPage");
+}
 
 const GlossaryHeader = () => <div className="glossaryHeader_container">
     <link href="https://fonts.googleapis.com/css2?family=Assistant&family=Poppins&display=swap" rel="stylesheet"></link>
     <div className="glossaryHeader_bot">
         <div className="glossary_button_container">
-            <p className="glossary_button_text glossary_button_text_1">Trees</p>
-            <p className="glossary_button_text glossary_button_text_2">Flowering Plant</p>
-            <p className="glossary_button_text glossary_button_text_3">Terminology</p>
+            <p className="glossary_button_text glossary_button_text_1" onClick={onClick1}>Trees</p>
+            <p className="glossary_button_text glossary_button_text_2" onClick={onClick2}>Flowering Plant</p>
+            <p className="glossary_button_text glossary_button_text_3" onClick={onClick3}>Terminology</p>
         </div>
     </div>
     <div className="glossaryHeader_top">
--- pages/TreeGlossaryPage/index.js
+++ pages/TreeGlossaryPage/index.js
@@ -2,13 +2,19 @@ import FindPlantButton from '../../Comps/Kozy/FindPlantButton';
 import GlossaryHeaderTree from '../../Comps/Aksel/GlossaryHeaderTree';
 import GlossaryTerm from '../../Comps/David/glossary_term';
 import './treeglossarypage.css';
+import Router from 'next/router'
+import { route } from 'next/dist/next-server/server/router';
 
 const tree1 = require("../../Imgs/aspidistra_elatior.png");
 const tree2 = require("../../Imgs/dwarf_juniper_tree.png");
 
+function result01(){
+    Router.push("/ResultPage")
+}
+
 const TreeGlossaryPage = () => <div>
-    <GlossaryTerm title="Aspidistra Elatior" placeholder={tree1}></GlossaryTerm>
-    <GlossaryTerm title="Dwarf Juniper Tree" placeholder={tree2}>></GlossaryTerm>
+    <GlossaryTerm title="Aspidistra Elatior" placeholder={tree1} onClick={result01}></GlossaryTerm>
+    <GlossaryTerm title="Dwarf Juniper Tree" placeholder={tree2} onClick={result01}></GlossaryTerm>
     <GlossaryTerm></GlossaryTerm>
     <GlossaryTerm></GlossaryTerm>
     <GlossaryTerm></GlossaryTerm>
