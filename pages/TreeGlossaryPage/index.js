07037c7a6bc4cfbee65b747e0e2cb6dd4890ea0f
Aksel <roughsedge.a@gmail.com>
2020-05-12 15:02:57 -0700

    Added trees to glossary w/ images

--- pages/TreeGlossaryPage/index.js
+++ pages/TreeGlossaryPage/index.js
@@ -6,8 +6,18 @@ import Router from 'next/router'
 import { route } from 'next/dist/next-server/server/router';
 import React, {useState, useEffect} from "react";
 
-const tree1 = require("../../Imgs/aspidistra_elatior.png");
-const tree2 = require("../../Imgs/dwarf_juniper_tree.png");
+const tree01 = require("../../Imgs/areca_palm.png");
+const tree02 = require("../../Imgs/aspidistra_elatior.png");
+const tree03 = require("../../Imgs/fiddle_leaf_fig_tree.png");
+const tree04 = require("../../Imgs/yucca_tree.png");
+const tree05 = require("../../Imgs/norfolk_tree.png");
+const tree06 = require("../../Imgs/madagascar_dragon_tree.png");
+const tree07 = require("../../Imgs/hoya_tree.png");
+const tree08 = require("../../Imgs/stromanthe_triostar.png");
+const tree09 = require("../../Imgs/rubber_fig_plant.png");
+const tree10 = require("../../Imgs/zamioculcas_zamiifolia.png");
+const tree11 = require("../../Imgs/parlour_palm.png");
+
 
 function result01(){
     Router.push("/ResultPage")
@@ -23,15 +33,17 @@ const TreeGlossaryPage = () => {
 
 return<div>
     <div className="glossary_effect_tree">
-    <GlossaryTerm title="Areca Palm" placeholder={tree1} onClick={result01}></GlossaryTerm>
-    <GlossaryTerm title="Dwarf Juniper Tree" placeholder={tree2} onClick={result01}></GlossaryTerm>
-    <GlossaryTerm></GlossaryTerm>
-    <GlossaryTerm></GlossaryTerm>
-    <GlossaryTerm></GlossaryTerm>
-    <GlossaryTerm></GlossaryTerm>
-    <GlossaryTerm></GlossaryTerm>
-    <GlossaryTerm></GlossaryTerm>
-    <GlossaryTerm></GlossaryTerm>
+    <GlossaryTerm title="Areca Palm" placeholder={tree01} onClick={result01}></GlossaryTerm>
+    <GlossaryTerm title="Aspidistra Elatior" placeholder={tree02} onClick={result01}></GlossaryTerm>
+    <GlossaryTerm title="Fiddle Leaf Fig Tree" placeholder={tree03} onClick={result01}></GlossaryTerm>
+    <GlossaryTerm title="Yucca Tree" placeholder={tree04} onClick={result01}></GlossaryTerm>
+    <GlossaryTerm title="Norfolk Tree" placeholder={tree05} onClick={result01}></GlossaryTerm>
+    <GlossaryTerm title="Madagascar Dragon Tree" placeholder={tree06} onClick={result01}></GlossaryTerm>
+    <GlossaryTerm title="Hoya Tree" placeholder={tree07} onClick={result01}></GlossaryTerm>
+    <GlossaryTerm title="Stromanthe Triostar" placeholder={tree08} onClick={result01}></GlossaryTerm>
+    <GlossaryTerm title="Rubber Fig Plant" placeholder={tree09} onClick={result01}></GlossaryTerm>
+    <GlossaryTerm title="Zamioculcas Zamiifolia" placeholder={tree10} onClick={result01}></GlossaryTerm>
+    <GlossaryTerm title="Parlour Palm" placeholder={tree11} onClick={result01}></GlossaryTerm>
     </div>
     <div className="glossary_spacer"></div>
     <GlossaryHeaderTree></GlossaryHeaderTree>
