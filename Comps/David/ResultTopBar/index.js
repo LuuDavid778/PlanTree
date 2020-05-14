07439f61ab23bc54b532acfd0a7d0625ea8e2fef
= <=>
2020-05-14 11:02:20 -0700

    Replaced the logo with png on the result page and tutorial page

--- Comps/David/ResultTopBar/index.js
+++ Comps/David/ResultTopBar/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './ResultTopBar.css'
 import Router from 'next/router';
 import HamburgerMenu from '../../David/hamburger_menu'
-const HorizontalLogo = require('../../../Imgs/horizontal_logo.svg');
+const HorizontalLogo = require('../../../Imgs/horizontal_logo.png');
 
 function HomePage(){
 Router.push("/")
