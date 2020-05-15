72a60fc66da65d3a6d5f122f1fa9410888a8353c
luudavid778 <luudavid778@gmail.com>
2020-05-14 17:48:45 -0700

    Trying to resolve result page..

--- pages/ResultPage/index.js
+++ pages/ResultPage/index.js
@@ -8,7 +8,7 @@ import PlantName from '../../Comps/David/resultpage_plantname'
 import ResultRestart from '../../Comps/David/result_restart'
 import './ResultPage.css'
 import  {useEffect} from 'react';
-import data, { checkResults } from '../../data'
+import { checkResults } from  "../../data"
 
 
 const waterdrop = require("../../Imgs/water_drop.svg")
