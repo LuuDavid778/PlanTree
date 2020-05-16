3f3830fa159c6abc2ac364225be1b50cf8c79be1
luudavid778 <luudavid778@gmail.com>
2020-05-16 00:46:54 -0700

    fixed some linking

--- Comps/David/glossary_hamburger/index.js
+++ Comps/David/glossary_hamburger/index.js
@@ -64,7 +64,7 @@ Router.push("/PlantPage")
 </div>
 
 <div className = "hamburger_menu_main_div" onClick = {()=>{
-Router.push("/Glossarypage")
+Router.push("/GlossaryPage")
 }}>
     <img src = {GlossaryIcon}></img>
     <h2>Glossary</h2>
