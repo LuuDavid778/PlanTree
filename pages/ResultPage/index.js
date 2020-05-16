39ce505df8068716440cee2e69e1f72f83498c09
David! <55604802+LuuDavid778@users.noreply.github.com>
2020-05-15 22:14:47 -0700

    Revert "David"

--- pages/ResultPage/index.js
+++ pages/ResultPage/index.js
@@ -1,6 +1,6 @@
 import ResultScreenTitle from '../../Comps/Aksel/ResultScreenTitle'
 import ResultScreenImg from '../../Comps/Aksel/ResultScreenImg'
-import ResultScreenQuickFacts from '../../Comps/David/ResultScreenQuickFacts'
+import ResultScreenQuickFacts from '../../Comps/Aksel/ResultScreenQuickFacts'
 import ResultTexts from '../../Comps/Kozy/ResultTexts'
 import ResultTopBar from '../../Comps/David/ResultTopBar'
 import ResultCard from '../../Comps/David/ResultCard'
@@ -9,19 +9,10 @@ import ResultRestart from '../../Comps/David/result_restart'
 import './ResultPage.css'
 import  {useEffect} from 'react';
 import {checkResults} from '../../data'
-import {data, changeData} from '../../data'
-import  '../../data'
+
 const waterdrop = require("../../Imgs/water_drop.svg")
 const trowel = require("../../Imgs/trowel.svg")
 
-
-
-const groweasy = require('../../Imgs/groweasy.svg');
-const growmedium = require('../../Imgs/growmedium.svg');
-const growhard = require('../../Imgs/growhard.svg');
-const watereasy = require('../../Imgs/watereasy.svg');
-const watermedium = require('../../Imgs/watermedium.svg');
-const waterhard = require('../../Imgs/waterhard.svg');
 
 const ResultPage = () => {
     {checkResults()}
@@ -74,16 +65,16 @@ return <div className = "ResultPage_cont">
 <div className = "ResultScreen_qf_cont">
 <div className = "result_box1">
 <div className = "ResultScreen_qf_boxes">
-<ResultScreenQuickFacts card = {data.card1} ></ResultScreenQuickFacts>
+<ResultScreenQuickFacts icona ={waterdrop} iconb ={waterdrop} iconc ={waterdrop} displaya = "block" displayb = "block" displayc = "block" text = "Needs to be watered once/week"></ResultScreenQuickFacts>
 </div>
 </div>
 <div className = "result_box2">
 <div className = "ResultScreen_qf_boxes">
-<ResultScreenQuickFacts card = {data.card2} ></ResultScreenQuickFacts>
+<ResultScreenQuickFacts icona ={trowel} iconb ={trowel} iconc ={trowel} displaya = "block" displayb = "block" displayc = "block" text = "Quite hard to take care of"></ResultScreenQuickFacts>
 </div>
 </div>
 <div  className = "result_box3">
-<ResultScreenQuickFacts card = {data.card3} ></ResultScreenQuickFacts>
+<ResultScreenQuickFacts></ResultScreenQuickFacts>
 </div>
 
 </div>
